4bbbb5bb0c80c214107470ed9aa71727f438bc83
Ru-Co-La <78157213+Ru-Co-La@users.noreply.github.com>
2022-07-19 14:41:30 +0200

    Comments added

--- l2H2P5.py
+++ l2H2P5.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import itertools
 from scipy.special import comb
 
-# Number of i.c.
+### Number of i.c.
 N = 10e5
-# gamma < 1/32
+### gamma < 1/32
 gamma = 1/64
 #
 AB = 1/2
@@ -14,12 +14,10 @@ CD = 1/8
 DE = 1/16
 EE = 1/16-gamma
 EA = gamma
-# Assume uniform distribution over [0,1] with dynamics x+ = 1/2x for x in [gamma,1] and x+ = 1/2(x+1) elsewhere
-# A = [1/2,1], B = [0,1/2)
+### Assume uniform distribution over [0,1] with dynamics x+ = 1/2x for x in [gamma,1] and x+ = 1/2(x+1) elsewhere
 v = (("AB",AB),("BC",BC), ("CD",CD), ("DE",DE),("EE",EE),("EA",EA))
 u = ("AB","BC","CD","DE","EE","EA")
 M = len(v)
-# Generate all possible outcomes of length N
 V_support = np.array(())
 Pmf_V = np.array(())
 Pmf_seen = np.zeros(M)
@@ -27,9 +25,7 @@ Pmf_exact_symbols = {}
 for i in range(1,M+1):
     index_mat = list(itertools.combinations(range(M), i))
     for chosen in index_mat:
-        #print(chosen)
         violation = sum([v[u][1] for u in range(M) if u not in chosen])
-        #print(violation)
         ### Probability of seeing all the possible N outcomes with sequences made up of any combination of the chosen
         outcome_probability = np.power((1 - violation), N)
         Pmf_exact_symbols[chosen] = outcome_probability
@@ -53,14 +49,14 @@ for i in range(1,M+1):
             Pmf_seen[i-1] -= Pmf_seen[j-1]*comb(M-j, i-j)
     print("Probability of seeing exactly ", i, " symbols ", Pmf_seen[i - 1])
 
-print("Pmf_seen ", sum(Pmf_seen))
-print("Pmf_exact_symbols ", sum(Pmf_exact_symbols.values()))
-print("Pmf_V ", sum(Pmf_V))
-#Sort arrays
+print("Pmf_seen sums to ", sum(Pmf_seen))
+print("Pmf_exact_symbols sums to ", sum(Pmf_exact_symbols.values()))
+print("Pmf_V sums to ", sum(Pmf_V))
+### Sort arrays
 p = V_support.argsort()
 V_support = V_support[p]
 Pmf_V = Pmf_V[p]
-
+### Plot stuff
 print("Support of the Violation")
 print(V_support)
 print("Probability mass function of the Violation")
